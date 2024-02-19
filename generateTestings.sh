87006e65f4f28bee043bec3867020be19d56a882
Ziosan13 <yuta.iland.13.nexus@gmail.com>
2024-02-19 04:50:12 +0100

    add: Add explanation and automation of generating tests

---
+++ generateTestings.sh
@@ -0,0 +1,2 @@
+javac -cp . -d classes test/RandomTest.java test/RandomTestingGenerator.java test/PairwiseTest.java test/PairwiseTestingGenerator.java test/Testing.java
+java -cp classes test.Testing
